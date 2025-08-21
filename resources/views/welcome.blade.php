15db2727cbd7381338d628e1a4fd28fd60ce6b7d
Michel BOUY <michelange@wanadoo.fr>
2025-08-21 20:07:33 +0200

    amélioration de la page d'accueil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,7 +56,7 @@
                     <p class="mb-1 ">Byblos</p>
                     <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Partageons nos lectures.
                 </div>
-                <div class="bg-[#fff2f2] dark:bg-[#1D0002] relative lg:-ms-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-e-lg! aspect-[335/376] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
+                <div class="bg-[#fff] dark:bg-[#1D0002] relative flex items-end lg:-ms-px -mb-px lg:mb-0 rounded-t-lg lg:rounded-t-none lg:rounded-e-lg! aspect-[335/376] lg:aspect-auto w-full lg:w-[438px] shrink-0 overflow-hidden">
                     <img src="{{ asset('storage/img/babel.jpg') }}" alt="Bibliothèque de Babylone">
                 </div>
             </main>
