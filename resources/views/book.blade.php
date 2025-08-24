e7fd7f788bec27048db3a495097e1d362e308169
Michel BOUY <michelange@wanadoo.fr>
2025-08-24 19:57:48 +0200

    ajout du menu etiquettes

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -2,7 +2,8 @@
     <div class="flex flex-col gap-2 xl:w-4/5 mr-5">
         <div class="">
             <p class="text-gray-500 underline mb-2">
-                <a href="{{ route('books.index') }}"><i class="fa-solid fa-caret-left"></i>&nbsp;Retour à la bibliothèque</a>
+                <a href="{{ route('books.index') }}"><i class="fa-solid fa-caret-left"></i>&nbsp;Retour à la
+                    bibliothèque</a>
             </p>
             <h1 class="font-bold text-3xl">
                 {{ $book->title }}
@@ -16,8 +17,6 @@
             <div class="basis-2/3 flex flex-col gap-5 text-justify py-2 px-8">
                 <p>{{ $book->description }} </p>
 
-                
-                
                 <div class="flex flex-row justify-between">
                     <div>
                         <a href="{{ route('books.download', $book) }}"
@@ -26,38 +25,39 @@
                         </a>
                     </div>
                     <div class="flex flex-row gap-2">
-                        @auth
-                        @if(auth()->user()->email === 'michelange@wanadoo.fr')
                         <a href="{{ route('books.edit', $book) }}"
-                        class="inline-block m-auto px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:bg-gray-200 hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
-                        Modifier
-                    </a>
-                                <form action="{{ route('books.destroy', $book) }}" method="GET" x-data="{ open: false }">
-                                    @csrf
-                                    @method('GET')
-                                    
-                                    <button type="button" class="inline-block m-auto px-5 py-1.5 text-red-800 dark:text-[#EDEDEC] border-[#19140035] hover:bg-red-800 hover:text-white hover:border-[#1915014a] border dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal" @click="open = true">
-                                        Supprimer
-                                    </button>
-                                    
-                                    <div x-show="open" class="fixed inset-0 flex items-center justify-center bg-black/50">
-                                        <div class="bg-white p-6 rounded shadow-lg">
-                                            <p class="mb-4">Voulez-vous vraiment supprimer ce livre ?</p>
-                                            <div class="flex justify-end gap-2">
-                                                <button @click="open = false" type="button" class="px-4 py-2 bg-gray-300 rounded">Annuler</button>
-                                                <button @click="$el.closest('form').submit()" type="button" class="px-4 py-2 bg-red-600 text-white rounded">Supprimer</button>
-                                            </div>
-                                        </div>
+                            class="inline-block m-auto px-5 py-1.5 dark:text-[#EDEDEC] border-[#19140035] hover:bg-gray-200 hover:border-[#1915014a] border text-[#1b1b18] dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal">
+                            Modifier
+                        </a>
+                        <form action="{{ route('books.destroy', $book) }}" method="GET" x-data="{ open: false }">
+                            @csrf
+                            @method('GET')
+
+                            <button type="button"
+                                class="inline-block m-auto px-5 py-1.5 text-red-800 dark:text-[#EDEDEC] border-[#19140035] hover:bg-red-800 hover:text-white hover:border-[#1915014a] border dark:border-[#3E3E3A] dark:hover:border-[#62605b] rounded-sm text-sm leading-normal"
+                                @click="open = true">
+                                Supprimer
+                            </button>
+
+                            <div x-show="open" class="fixed inset-0 flex items-center justify-center bg-black/50">
+                                <div class="bg-white p-6 rounded shadow-lg">
+                                    <p class="mb-4">Voulez-vous vraiment supprimer ce livre ?</p>
+                                    <div class="flex justify-end gap-2">
+                                        <button @click="open = false" type="button"
+                                            class="px-4 py-2 bg-gray-300 rounded">Annuler</button>
+                                        <button @click="$el.closest('form').submit()" type="button"
+                                            class="px-4 py-2 bg-red-600 text-white rounded">Supprimer</button>
                                     </div>
-                                </form>
-                                @endif
-                                @endauth
-                    </div>                                        
+                                </div>
+                            </div>
+                        </form>
+
+                    </div>
                 </div>
             </div>
         </div>
         <livewire:book-tags :book="$book" />
     </div>
-    
+
 
 </x-layouts.app>
---
+++ resources/views/flux/icon/tag-icon.blade.php
@@ -0,0 +1,34 @@
+{{-- Credit: Lucide (https://lucide.dev) --}}
+
+@props([
+    'variant' => 'outline',
+])
+
+@php
+    if ($variant === 'solid') {
+        throw new \Exception('The "solid" variant is not supported in Lucide.');
+    }
+
+    $classes = Flux::classes('shrink-0')->add(
+        match ($variant) {
+            'outline' => '[:where(&)]:size-6',
+            'solid' => '[:where(&)]:size-6',
+            'mini' => '[:where(&)]:size-5',
+            'micro' => '[:where(&)]:size-4',
+        },
+    );
+
+    $strokeWidth = match ($variant) {
+        'outline' => 2,
+        'mini' => 2.25,
+        'micro' => 2.5,
+    };
+@endphp
+
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+    class="lucide lucide-tag-icon lucide-tag">
+    <path
+        d="M12.586 2.586A2 2 0 0 0 11.172 2H4a2 2 0 0 0-2 2v7.172a2 2 0 0 0 .586 1.414l8.704 8.704a2.426 2.426 0 0 0 3.42 0l6.58-6.58a2.426 2.426 0 0 0 0-3.42z" />
+    <circle cx="7.5" cy="7.5" r=".5" fill="currentColor" />
+</svg>
