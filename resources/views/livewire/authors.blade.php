97164c843e09431235f952dbdaa70011648db770
Michel BOUY <michelange@wanadoo.fr>
2025-08-27 11:13:20 +0200

    mise en forme auteurs

--- resources/views/livewire/authors.blade.php
+++ resources/views/livewire/authors.blade.php
@@ -1,5 +1,4 @@
-<div class="max-w-3xl mx-auto p-4">
-
+<div class=" mx-auto p-4">
     <!-- Barre de recherche -->
     <div class="relative mb-4">
         <input type="text" wire:model.live="query" placeholder="Rechercher par auteur ou titre..."
@@ -15,37 +14,40 @@
     </div>
 
     <!-- Liste des auteurs et leurs livres -->
-    @foreach ($authors as $author)
-        <div class="mb-6">
-            <h2 class="text-lg font-bold mb-2">
-                {{ $author->author }}
-            </h2>
-
-            <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 items-baseline">
-                @foreach ($books as $book)
-                    @if ($book->lastName === $author->lastName)
-                        <div class="flex flex-col gap-1 justify-center">
-                            <a href="{{ route('books.show', $book->id) }}">
-                                <img class="h-[10vh] w-auto object-cover border-2 border-gray-300 rounded"
+    <div class="grid lg:grid-cols-2 xl:grid-cols-3 gap-4">
+        @foreach ($authors as $author)
+            <div class="mb-6 bg-gray-100 p-3">
+                <h2 class="text-lg font-bold mb-2">
+                    {{ $author->author }}
+                </h2>
+    
+                <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+                    @foreach ($books as $book)
+                        @if ($book->lastName === $author->lastName)
+                            <div class="flex flex-col gap-1 justify-center">
+                                <a href="{{ route('books.show', $book->id) }}">
+                                    <img class="h-[20vh] sm:h-[10vh] w-auto object-cover m-auto"
                                     src="{{ asset('storage/' . $book->cover) }}" alt="{{ $book->title }}" title="Cliquer pour plus de détail">
-                            </a>
-                            <p class="text-sm truncate">{{ $book->title }}</p>
-                        </div>
-                        <!-- Icons visibles mobile -->
-                        <div class="flex justify-center gap-4 mt-2 text-gray-700 sm:hidden">
-                            <a href="{{ route('books.prepareDownload', $book->id) }}"
-                                class="text-gray-700 hover:text-black transition">
-                                <i class="fa-solid fa-download text-xl"></i>
-                            </a>
-                            <a href="{{ route('books.show', $book->id) }}"
-                                class="text-gray-700 hover:text-black transition">
-                                <i class="fa-regular fa-eye text-xl"></i>
-                            </a>
-                        </div>
-                    @endif
-                @endforeach
+                                </a>
+                                <p class="collapse sm:visible text-sm truncate" title="{{ $book->title }}">{{ $book->title }}</p>
+                                <!-- Icons visibles mobile -->
+                                <div class="flex justify-center gap-4 mb-3 text-gray-700 sm:hidden items-end">
+                                    <a href="{{ route('books.prepareDownload', $book->id) }}"
+                                        class="text-gray-700 hover:text-black transition">
+                                        <i class="fa-solid fa-download text-xl"></i>
+                                    </a>
+                                    <a href="{{ route('books.show', $book->id) }}"
+                                        class="text-gray-700 hover:text-black transition">
+                                        <i class="fa-regular fa-eye text-xl"></i>
+                                    </a>
+                                </div>
+                            </div>
+                            <p class="sm:collapse visible text-sm sm:truncate" title="{{ $book->title }}">{{ $book->title }}</p>
+                        @endif
+                    @endforeach
+                </div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 
 </div>
