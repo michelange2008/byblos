870f04be59752ab2b5da073a234ee798fa993f1f
Michel BOUY <michelange@wanadoo.fr>
2025-08-28 07:45:20 +0200

    correction du swipe qui ne marchait pas

--- resources/views/components/book-navigation.blade.php
+++ resources/views/components/book-navigation.blade.php
@@ -16,20 +16,29 @@
     handleTouchStart(event) { this.startX = event.touches[0].clientX },
     handleTouchEnd(event) {
         this.endX = event.changedTouches[0].clientX;
-        if (this.startX - this.endX > 50) this.nextBook();
-        if (this.endX - this.startX > 50) this.prevBook();
+        if (this.startX - this.endX > 50) {
+            this.nextBook();
+        }
+        if (this.endX - this.startX > 50) {
+            this.prevBook();
+        }
     }
-}" @touchstart="handleTouchStart($event)" @touchend="handleTouchEnd($event)">
+}" 
+class="w-full h-full" 
+@touchstart.window="handleTouchStart($event)" 
+@touchend.window="handleTouchEnd($event)">
 
     <!-- boutons navigation desktop -->
-    <div class="flex justify-between">
-        <button @click="prevBook()" class="hidden md:inline-block px-3 py-1 bg-gray-200 rounded"
-            :disabled="currentIndex === 0">
+    <div class="flex justify-between mt-6">
+        <button @click="prevBook()" 
+                class="hidden md:inline-block px-3 py-1 bg-gray-200 rounded"
+                :disabled="currentIndex === 0">
             ← Livre précédent
         </button>
 
-        <button @click="nextBook()" class="hidden md:inline-block px-3 py-1 bg-gray-200 rounded"
-            :disabled="currentIndex === books.length - 1">
+        <button @click="nextBook()" 
+                class="hidden md:inline-block px-3 py-1 bg-gray-200 rounded"
+                :disabled="currentIndex === books.length - 1">
             Livre suivant →
         </button>
     </div>
