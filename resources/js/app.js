46497eec0ea23645262374c84462d559444fbdb9
Michel BOUY <michelange@wanadoo.fr>
2025-09-13 18:28:07 +0200

    modif chargement icones

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,13 @@
 // resources/js/app.js
-import { 
+import {
   createIcons,
   Download, Save, Eye, Pencil, Trash, Trash2,
   BookOpenText, House, UsersRound, UserPlus, Plus, Undo2, Tag
 } from 'lucide';
 
-createIcons({
-  icons: {
-    Download, Save, Eye, Pencil, Trash, Trash2,
-    BookOpenText, House, UsersRound, UserPlus, Plus, Undo2, Tag
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  createIcons({
+    icons: { Download, Save, Eye, Pencil, Trash, Trash2, BookOpenText, House, UsersRound, UserPlus, Plus, Undo2, Tag }
+  });
 });
 
