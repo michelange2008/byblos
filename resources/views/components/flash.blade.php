ca412ae9c0a0ab710f66b6862fd59151b7563a9e
Michel BOUY <michelange@wanadoo.fr>
2025-10-12 18:09:48 +0200

    build

--- resources/views/components/flash.blade.php
+++ resources/views/components/flash.blade.php
@@ -7,7 +7,7 @@
 @endif
 
 @if (session('error'))
-    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4">
+    <div class="bg-red-200 border border-red-500 text-red-900 px-4 py-3 rounded mb-4">
         {{ session('error') }}
     </div>
 @endif
